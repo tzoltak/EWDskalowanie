de8054f0ec1cb898d282355e9b4827087b1983c4
g.golonka <g.golonka@ibe.edu.pl>
2014-05-07 15:25:55 +0200

    Poprawki kodu dla funkcji zapisz_parametry_skalowania oraz korekta funkcji pobierz_parametru_skalowania.

--- R/pobierz_parametry_skalowania.R
+++ R/pobierz_parametry_skalowania.R
@@ -27,7 +27,7 @@
 #' str(ret[[1]])
 #' @export
 pobierz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, 
-                                         opis_skalowania='%',zrodloDanychODB = 'ewd_grzes'){
+                                         opis_skalowania='.*', zrodloDanychODB = 'ewd_grzes'){
   
   if(is.null(nazwa_skali) & is.null(id_testu)  ){
     stop("Nazwa skali oraz id testu nie mogą mieć jednocześnie wartości null.")
@@ -35,28 +35,29 @@ pobierz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL,
   
   where = "where"
   if( !is.null(nazwa_skali) ){
-    where = paste0(where," nazwa = '",nazwa_skali,"'" )
+    where = paste0(where," nazwa = '", nazwa_skali, "'" )
     
     if(!is.null(id_testu)){
-      where = paste0(where, " and id_testu = ",id_testu,"")
+      where = paste0(where, " and id_testu = ", id_testu, "")
     }
   } else {
-    where = paste0(where, " id_testu = ",id_testu,"")
+    where = paste0(where, " id_testu = ", id_testu, "")
   }
-  where = paste0(where," and SA.opis LIKE '",opis_skalowania,"'")
+  where = paste0(where, " and SA.opis ~* '", opis_skalowania, "'")
   
-  joiny = paste0("
+  joiny =       "
                 from skale AS S
                 JOIN skale_elementy AS SE USING(id_skali)
                 JOIN skalowania_elementy AS SAE USING(id_skali,kolejnosc)
                 JOIN skalowania AS SA USING(id_skali,skalowanie)
-                ")
+                "
   
+                # SA.id_skali, kolejnosc, 
   zapytanie1 = paste0("
                 select  
-                SA.id_skali, kolejnosc, id_kryterium, id_pseudokryterium,
+                id_kryterium, id_pseudokryterium,
                 SAE.model,SAE.parametr,SAE.wartosc, SA.skalowanie
-                      ", joiny , where)
+                      ", joiny , where, '\n ORDER BY skalowanie, kolejnosc, parametr')
   
   zapytanie2 = paste0("
                 select distinct 
@@ -73,9 +74,9 @@ pobierz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL,
   P = odbcConnect(zrodloDanychODB)
   tryCatch({
     
-    tablicaDanych = sqlQuery(P, gsub(" "," ",zapytanie1))
-    opisSkalowan =  sqlQuery(P, gsub(" "," ",zapytanie2))
-    skale =  sqlQuery(P, gsub(" "," ",zapytanie3))
+    tablicaDanych = sqlQuery(P, gsub(" ", " ", zapytanie1))
+    opisSkalowan =  sqlQuery(P, gsub(" ", " ", zapytanie2))
+    skale =  sqlQuery(P, gsub(" ", " ", zapytanie3))
     odbcClose(P)
     
   },
@@ -89,27 +90,28 @@ pobierz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL,
     return(NULL)
   }
   
-  
   ret = list()
   
   for(ind in 1:nrow(opisSkalowan)){
-    ret[[1]] = tablicaDanych[
-                            tablicaDanych[,"skalowanie"] == opisSkalowan[,1], 
+    ret[[ind]] = tablicaDanych[
+                            tablicaDanych[, "skalowanie"] == opisSkalowan[,ind], 
                             colnames(tablicaDanych) != "skalowanie"
                             ]
     
     for(k in 2:ncol(opisSkalowan)){
-      attr(ret[[1]],colnames(opisSkalowan)[k]) = as.character(opisSkalowan[ind,k])
+      attr(ret[[ind]], colnames(opisSkalowan)[k]) = as.character(opisSkalowan[ind, k])
     }
   }
   
-  if(nrow(skale)>1){
-    warning("Więcej niż jedna skala przpisana do wyników. 
-            Argumentami zwracanej listy są wartości dla pierwszej skali.")
+  if( nrow(skale) > 1 ){
+    cat("Skale spełniające kryteria wyszukiwania: \n")
+    print(skale)
+    
+    stop("Więcej niż jedna skala przpisana do wyników.")
   }
   
-  for(k in 1:ncol(skale)){
-    attr(ret,colnames(skale)[k]) = as.character(skale[1,k])
+  for( k in 1:ncol(skale) ){
+    attr(ret, colnames(skale)[k]) = as.character(skale[1, k])
   }
   
   return(ret)
--- R/zapisz_parametry_skalowania.R
+++ R/zapisz_parametry_skalowania.R
@@ -13,16 +13,16 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
     
     # zapytanie
     if(!is.null(nazwa_skali)){
-      zapytanie = paste0("select id_skali from skale where nazwa = '",nazwa_skali,"'")
+      zapytanie = paste0("select id_skali from skale where nazwa = '", nazwa_skali, "'")
       if(!is.null(id_testu)){
-        zapytanie = paste0(zapytanie, " and id_testu = ",id_testu,"")
+        zapytanie = paste0(zapytanie, " and id_testu = ", id_testu, "")
       }
     } else{
-      zapytanie = paste0("select id_skali from skale where id_testu = '",id_testu,"'")
+      zapytanie = paste0("select id_skali from skale where id_testu = '", id_testu, "'")
     }
     skaleZap = sqlQuery(P, zapytanie)
     
-    zapytanie = paste0("SELECT count(*) from sl_estymacje_parametrow where estymacja = '",estymacja,"'")
+    zapytanie = paste0("SELECT count(*) from sl_estymacje_parametrow where estymacja = '", estymacja, "'")
     estymacjeNumZap = sqlQuery(P, zapytanie)
     
     zapytanie = "SELECT MAX(skalowanie) from skalowania"
@@ -40,7 +40,7 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
   
   if( length(skaleZap)==0 ){
     stop("Nie ma w bazie skali.")
-  } else if(length(skaleZap)!=1){
+  } else if(length(skaleZap) != 1){
     stop("Skala określona niejednoznacznie.")
   }
   idSkali = skaleZap[1,1]
@@ -55,14 +55,14 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
     sqlQuery(P, "BEGIN;")
     
     insert = paste0("INSERT INTO skalowania (skalowanie, opis , estymacja,id_skali) 
-                    VALUES (",maxSkalowanieZap+1,",\'",opis,"\', \'",estymacja,"\',",idSkali,") 
+                    VALUES (", maxSkalowanieZap+1, ",\'",opis, "\', \'", estymacja, "\',", idSkali, ") 
                     ;")
     sqlQuery(P, insert)
     
     parBy = parametry[parametry[,1]=="by",]
     parTreshold= parametry[parametry[,1]=="threshold",]
     
-    kryteriaBy = as.numeric (sapply(strsplit(parBy[,"zmienna2"], "_"), function(x) x[length(x)]  ))
+    kryteriaBy = as.numeric (sapply(strsplit(parBy[, "zmienna2"], "_"), function(x) x[length(x)]  ))
     kryteriaTres = as.numeric (sapply(strsplit(parTreshold[,"zmienna1"], "_"), function(x) x[length(x)]  ))
     
     liczbaParam = table(kryteriaTres)
@@ -77,50 +77,50 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
         
         # insert do tabeli skale_elementy
         insSkale =  paste0("INSERT INTO skale_elementy (kolejnosc,id_skali,id_kryterium)
-                           VALUES(",iter,",",idSkali,",",names(liczbaParam)[k],")"
+                           VALUES(", iter, ",", idSkali, ",", names(liczbaParam)[k],")"
         )
         sqlQuery(P, insSkale)
         
         insSkalowania =  paste0("INSERT INTO skalowania_elementy  (id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi)
                                 VALUES (",idSkali,
-                                ",",iter,",",
+                                ",", iter, ",",
                                 maxSkalowanieZap+1,
-                                " , 'dyskryminacja' ,","'2PL'",", ",
-                                parBy[kryteriaBy==kryt,"wartosc"],",'')"
+                                " , 'dyskryminacja' ,", "'2PL'", ", ",
+                                parBy[ kryteriaBy==kryt, "wartosc"], ",'')"
                                 )
         sqlQuery(P, insSkalowania)
         
         insSkalowania = paste0("INSERT INTO skalowania_elementy  (id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi) 
-                               VALUES (",idSkali,
-                               ",",iter,",",
+                               VALUES (", idSkali,
+                               ",", iter,",",
                                maxSkalowanieZap+1,
-                               " , 'trudność' ,","'2PL'",", ",
-                               parTreshold[kryteriaTres==kryt,"wartosc"]/parBy[kryteriaTres==kryt,"wartosc"],
+                               " , 'trudność' ,", "'2PL'", ", ",
+                               parTreshold[kryteriaTres==kryt, "wartosc"]/parBy[kryteriaTres==kryt, "wartosc"],
                                ",'')"
                                )
       } else if ( liczbaParam[k] > 1 ){ # model GRM
         
         insSkale =  paste0("INSERT INTO skale_elementy (kolejnosc,id_skali,id_kryterium)
-                           VALUES(",iter,",",idSkali,",",names(liczbaParam)[k],")"
+                           VALUES(", iter, ",",idSkali, ",", names(liczbaParam)[k], ")"
         )
         sqlQuery(P, insSkale)
         
-        dyskryminacja = parBy[kryteriaBy==kryt,"wartosc"]
+        dyskryminacja = parBy[kryteriaBy==kryt, "wartosc"]
         insSkalowania =  paste0("INSERT INTO skalowania_elementy  (id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi)
-                                VALUES (",idSkali,
-                                ",",iter,",",
+                                VALUES (", idSkali,
+                                ",", iter,",",
                                 maxSkalowanieZap+1,
-                                " , 'dyskryminacja' ,","'GRM'",", ",
-                                dyskryminacja,",'')"
+                                " , 'dyskryminacja' ,", "'GRM'", ", ",
+                                dyskryminacja, ",'')"
                                 )
         sqlQuery(P, insSkalowania)
         
         indTres = which(kryteriaTres==kryt)
-        srednia = mean(parTreshold[indTres,"wartosc"])
+        srednia = mean(parTreshold[indTres, "wartosc"])
         
         insSkalowania = paste0("INSERT INTO skalowania_elementy  (id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi) 
-                               VALUES (",idSkali,
-                               ",",iter,",",
+                               VALUES (", idSkali,
+                               ",", iter,",",
                                maxSkalowanieZap+1,
                                " , 'trudność' ,","'2PL'",", ",
                                srednia,
@@ -131,18 +131,18 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
         
         for(m in indTres ){
           
-          nazwaPar = paste0("k",which(m==indTres))
+          nazwaPar = paste0("k", which(m==indTres))
           
           if( ! nazwaPar %in% nazwyParametrow ){
-            sqlQuery(P, paste0("INSERT INTO sl_parametry(parametr,opis) values ('",nazwaPar,"','kn(GRM) - patrz opis parametru k1')")  )
+            sqlQuery(P, paste0("INSERT INTO sl_parametry(parametr,opis) values ('", nazwaPar, "','kn(GRM) - patrz opis parametru k1')")  )
           }
           
           insSkalowania = paste0("INSERT INTO skalowania_elementy  (id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi)
-                                 VALUES (",idSkali,
-                                 ",",iter,",",
-                                 maxSkalowanieZap+1,
-                                 " , nazwaPar ,","'GRM'",", ",
-                                 parTreshold[m,"wartosc"]/parBy[kryteriaBy==kryt,"wartosc"] - srednia,
+                                 VALUES (", idSkali,
+                                 ",", iter, ",",
+                                 maxSkalowanieZap + 1,
+                                 " , nazwaPar ,", "'GRM'", ", ",
+                                 parTreshold[m, "wartosc"]/parBy[kryteriaBy==kryt, "wartosc"] - srednia,
                                  ",'')"
                                  )
           sqlQuery(P, insSkalowania)
