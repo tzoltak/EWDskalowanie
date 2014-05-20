79a49373fad5c7ec4bf96468ba14823ab7d3a33c
tzoltak <t.zoltak@ibe.edu.pl>
2014-05-20 13:51:54 +0200

    Reizja funkcji zapisz_parametry_skalowania().

--- R/zapisz_parametry_skalowania.R
+++ R/zapisz_parametry_skalowania.R
@@ -1,14 +1,14 @@
-#' @title Zapisywanie parametrów skalowań.
+#' @title Zapisywanie parametrow skalowan.
 #' @description
 #' Funkcja zapisuje parametry skalowania do bazy danych
 #' @param nazwa_skali nazwa skali. Kiedy nazwa skali przymuje wartość NULL nie jest brana pod 
 #' uwagę w wyszukiwaniu. Wartość domyślna to NULL.
-#' @param id_testu id testu.Kiedy id testu przymuje wartość NULL nie jest brana pod 
-#' uwagę w wyszukiwaniu. artość domyślna to NULL.
+#' @param id_testu id testu. Kiedy id testu przymuje wartość NULL nie jest brana pod 
+#' uwagę w wyszukiwaniu. Wartość domyślna to NULL.
 #' @param parametry ramka danych o strukturze zgodnej z tą, w jakiej zwraca oszacowania parametrów funkcja skaluj().
 #' @param opis opis estymacji
-#' @param estymacja parametr opisujący estymację, która jest elementem tablicy sl_estymacje_parametrow.
-#' @param zrodloDanychODB string określający źródło danych. Wartość domyślna to 'EWD'
+#' @param estymacja parametr opisujący metodę estymacji - musi być jedną z wartości występujących w tablicy sl_estymacje_parametrow.
+#' @param zrodloDanychODB string określający źródło danych. Wartość domyślna to 'EWD'.
 #' @return Funkcja nie zwraca żadnej wartości.
 #' @export
 zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, parametry, opis, estymacja, zrodloDanychODBC='ewd_grzes') {
@@ -22,10 +22,10 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
             if(!is.null(nazwa_skali)){
               zapytanie = paste0("SELECT id_skali FROM skale where nazwa = '", nazwa_skali, "'")
               if(!is.null(id_testu)){
-                zapytanie = paste0(zapytanie, " and id_testu = ", id_testu, "")
+                zapytanie = paste0(zapytanie, " AND id_testu = ", id_testu, "")
               }
             } else{
-              zapytanie = paste0("SELECT id_skali FROM skale where id_testu = '", id_testu, "'")
+              zapytanie = paste0("SELECT id_skali FROM skale WHERE id_testu = '", id_testu, "'")
             }
             skaleZap = bezpieczne_sqlQuery(P, zapytanie)
             
@@ -38,18 +38,18 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
             }
             idSkali = skaleZap$id_skali[1]
             
-            zapytanie = paste0("SELECT count(*) FROM sl_estymacje_parametrow where estymacja = '", estymacja, "'")
+            zapytanie = paste0("SELECT count(*) FROM sl_estymacje_parametrow WHERE estymacja = '", estymacja, "'")
             estymacjeNumZap = bezpieczne_sqlQuery(P, zapytanie)
             
-            zapytanie = "SELECT MAX(skalowanie) FROM skalowania"
+            zapytanie = "SELECT max(skalowanie) FROM skalowania"
             maxSkalowanieZap = as.numeric(bezpieczne_sqlQuery(P, zapytanie))
             
             zapytanie = "SELECT parametr FROM sl_parametry"
-            nazwyParametrow = bezpieczne_sqlQuery(P, zapytanie)[,1]
+            nazwyParametrow = bezpieczne_sqlQuery(P, zapytanie)$parametr
             
-            zapytanie = paste0("SELECT GREATEST(MAX(skalowanie),count(*)) + 1
+            zapytanie = paste0("SELECT greatest(max(skalowanie), count(*)) + 1 AS skalowanie
                                FROM skalowania WHERE id_skali = ", idSkali)
-            numerSkalowania = bezpieczne_sqlQuery(P, zapytanie)[1,1]
+            numerSkalowania = bezpieczne_sqlQuery(P, zapytanie)$skalowanie
             
             odbcClose(P)
           },
@@ -58,46 +58,40 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
             stop(e)
           }
         )
-  
-  
-  
   if( estymacjeNumZap == 0 ){
-    stop("Podanej estymacji nie ma w bazie danych.")
+    stop("Podanej metody estymacji nie ma w bazie danych.")
   }
   
   P = odbcConnect(zrodloDanychODBC)
-  
   tryCatch({
-          parBy = parametry[parametry[, 1]=="by",]
-          parTreshold= parametry[parametry[, 1]=="threshold",]
+          parBy = parametry[parametry$typ=="by", ]
+          parTreshold= parametry[parametry$typ=="threshold", ]
           
-          by = wydziel_kryteria_pseudokryteria(parBy[, "zmienna2"])
+          by = wydziel_kryteria_pseudokryteria(parBy$zmienna2)
           kryteriaBy = by$kryteria
           pseudokryteriaBy = by$pseudokryteria
-          
-          
-          tres = wydziel_kryteria_pseudokryteria(parTreshold[, "zmienna1"])
+           
+          tres = wydziel_kryteria_pseudokryteria(parTreshold$zmienna1)
           kryteriaTres = tres$kryteria
           pseudokryteriaTres = tres$pseudokryteria
           
           liczbaParam = table(kryteriaTres)
           
-          zapytanie = paste0("select kolejnosc,id_kryterium,id_pseudokryterium from skale_elementy 
-                             where id_skali =", idSkali)
-          
+          zapytanie = paste0("SELECT kolejnosc, id_kryterium, id_pseudokryterium FROM skale_elementy 
+                             WHERE id_skali =", idSkali)
           skaleElementy = bezpieczne_sqlQuery(P, zapytanie)
-          kryteriaBaza = na.omit(skaleElementy[, "id_kryterium"])
-          pseudokryteriaBaza = na.omit(skaleElementy[, "id_pseudokryterium"])
+          kryteriaBaza = na.omit(skaleElementy$id_kryterium)
+          pseudokryteriaBaza = na.omit(skaleElementy$id_pseudokryterium)
           
-          sprawdz_zgodnosc_kryteriow(kryteriaTres,kryteriaBy, "kryteria 'treshold'", "kryteria 'by'")
-          sprawdz_zgodnosc_kryteriow(kryteriaBy,kryteriaTres, "kryteria 'by'", "kryteria 'treshold'")
-          sprawdz_zgodnosc_kryteriow(kryteriaTres,kryteriaBaza, "kryteria z parametru", "kryteria z bazy",error=FALSE)
-          sprawdz_zgodnosc_kryteriow(kryteriaBaza,kryteriaTres, "kryteria z bazy", "kryteria z parametru")
+          sprawdz_zgodnosc_kryteriow(kryteriaTres, kryteriaBy  , "kryteria 'treshold'" , "kryteria 'by'")
+          sprawdz_zgodnosc_kryteriow(kryteriaBy  , kryteriaTres, "kryteria 'by'"       , "kryteria 'treshold'")
+          sprawdz_zgodnosc_kryteriow(kryteriaTres, kryteriaBaza, "kryteria z parametru", "kryteria z bazy")
+          sprawdz_zgodnosc_kryteriow(kryteriaBaza, kryteriaTres, "kryteria z bazy"     , "kryteria z parametru", error=FALSE)
           
-          sprawdz_zgodnosc_kryteriow(pseudokryteriaTres,pseudokryteriaBy, "pseudokryteria 'treshold'", "pseudokryteria 'by'")
-          sprawdz_zgodnosc_kryteriow(pseudokryteriaBy,pseudokryteriaTres, "pseudokryteria 'by'", "pseudokryteria 'treshold'")
-          sprawdz_zgodnosc_kryteriow(pseudokryteriaTres,pseudokryteriaBaza, "pseudokryteria z parametru funkcji", "pseudokryteria z bazy", error=FALSE)
-          sprawdz_zgodnosc_kryteriow(pseudokryteriaBaza,pseudokryteriaTres, "pseudokryteria z bazy", "pseudokryteria z parametru funkcji")
+          sprawdz_zgodnosc_kryteriow(pseudokryteriaTres, pseudokryteriaBy  , "pseudokryteria 'treshold'"         , "pseudokryteria 'by'")
+          sprawdz_zgodnosc_kryteriow(pseudokryteriaBy  , pseudokryteriaTres, "pseudokryteria 'by'"               , "pseudokryteria 'treshold'")
+          sprawdz_zgodnosc_kryteriow(pseudokryteriaTres, pseudokryteriaBaza, "pseudokryteria z parametru funkcji", "pseudokryteria z bazy")
+          sprawdz_zgodnosc_kryteriow(pseudokryteriaBaza, pseudokryteriaTres, "pseudokryteria z bazy"             , "pseudokryteria z parametru funkcji", error=FALSE)
           
           bezpieczne_sqlQuery(P, "BEGIN;")
           
@@ -106,25 +100,22 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
           bezpieczne_sqlQuery(P, insert)
           
           for(k in 1:length(liczbaParam)){
-            
             krytNum = as.numeric(names(liczbaParam)[k])
             
             kolejnoscTemp = NULL
             if(krytNum %in% kryteriaBaza){
-              kolejnoscTemp = skaleElementy[ grepl(TRUE, skaleElementy[, "id_kryterium"] == krytNum), ][1]
+              kolejnoscTemp = skaleElementy$kolejnosc[ skaleElementy$id_kryterium == krytNum ]
             } else if(krytNum %in% pseudokryteriaBaza){
-              kolejnoscTemp = skaleElementy[ grepl(TRUE, skaleElementy[, "id_pseudokryterium"] == krytNum), ][1]
+              kolejnoscTemp = skaleElementy$kolejnosc[ skaleElementy$id_pseudokryterium == krytNum ]
             }
             
             # model 2PL
             if( liczbaParam[k]== 1 ){
-              
-              insSkalowania =  paste0("INSERT INTO skalowania_elementy  (id_elementu,id_skali,kolejnosc,
-                                      skalowanie,parametr,model,wartosc,uwagi,bl_std)
-                                      VALUES (nextval('skalowania_elementy_id_elementu_seq'),", idSkali,
-                                      ",", kolejnoscTemp,",", numerSkalowania,
-                                      " , 'dyskryminacja' ,", "'2PL'", ", ",
-                                      parBy[kryteriaBy==krytNum,"wartosc"], ",'',", parBy[kryteriaBy==krytNum, "S.E."], ")"
+              insSkalowania = paste0("INSERT INTO skalowania_elementy (id_elementu, id_skali, kolejnosc, skalowanie, parametr, model, wartosc, uwagi, bl_std)
+                                      VALUES (nextval('skalowania_elementy_id_elementu_seq'), ", idSkali,
+                                      ", ", kolejnoscTemp, ", ", numerSkalowania,
+                                      ", 'dyskryminacja', ", "'2PL'", ", ",
+                                      parBy$wartosc[kryteriaBy==krytNum], ", '', ", parBy$"S.E."[kryteriaBy==krytNum], ")"
                                       )
               bezpieczne_sqlQuery(P, insSkalowania)
               
@@ -140,7 +131,6 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
               bezpieczne_sqlQuery(P, insSkalowania)
               
             } else if ( liczbaParam[k] > 1 ){ # model GRM
-              
               dyskryminacja = parBy[kryteriaBy==krytNum,"wartosc"]
               insSkalowania =  paste0("INSERT INTO skalowania_elementy  (id_elementu,id_skali,kolejnosc,skalowanie,parametr,model,wartosc,uwagi,bl_std)
                                       VALUES (nextval('skalowania_elementy_id_elementu_seq'),", idSkali,
@@ -165,7 +155,6 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
               bezpieczne_sqlQuery(P, insSkalowania)
                       
               for(m in indTres ){
-                
                 nazwaPar = paste0("k", which(m==indTres))
                 
                 if( ! nazwaPar %in% nazwyParametrow ){
@@ -195,7 +184,7 @@ zapisz_parametry_skalowania <- function(nazwa_skali=NULL, id_testu=NULL, paramet
         )
   invisible(NULL)
 }
-#' @title Bezpieczna funkcja do wykonywania poleceń SQL.
+#' @title Bezpieczna funkcja do wykonywania polecen SQL.
 #' @description
 #' Funkcja wykonuje polecenie SQL. W przypadku niepowodzenia wyświetla błąd zwracany przez bazę danych.
 #' @param channel połączenie z bazą danych zwracane przez funkcję \code{\link{odbcConnect}}.
@@ -209,7 +198,7 @@ bezpieczne_sqlQuery <- function(channel, query){
   }
   return (ret)
 }
-#' @title Sprawdzanie zgodności kryteriów.
+#' @title Sprawdzanie zgodnosci kryteriow.
 #' @description
 #' Funkcja sprawdza, czy wektor kryteriów kryt1 jest zawarty w zbiorze kryteriów kryt2.
 #' @param kryt1 wektor liczb opisujący kryteria
@@ -219,13 +208,13 @@ bezpieczne_sqlQuery <- function(channel, query){
 #' @param error jeżeli TRUE to funkcje wyświetla błąd w przypadku, gdy któryś z elementów kryt1
 #' nie należy do kryt2.
 #' @return Funkcja nie zwraca żadnej wartości.
-sprawdz_zgodnosc_kryteriow <- function(kryt1, kryt2,nazwa1,nazwa2, error = TRUE){
+sprawdz_zgodnosc_kryteriow <- function(kryt1, kryt2, nazwa1, nazwa2, error=TRUE){
   if(  length(fInds <- which( ! unique(kryt1) %in% unique(kryt2))) !=0  ){
     if(error){
       stop(paste0(nazwa1, " i ", nazwa2," nie pokrywają się.",
                   " Brakujące ", nazwa1, ":\n"),
            paste(unique(kryt1)[fInds], collapse="\n"))
-    } else{
+    } else {
       warning(paste0(nazwa1," i ", nazwa2," nie pokrywają się.",
                      " Brakujące ", nazwa1,":\n"),
               paste(unique(kryt1)[fInds], collapse="\n"))
@@ -233,31 +222,30 @@ sprawdz_zgodnosc_kryteriow <- function(kryt1, kryt2,nazwa1,nazwa2, error = TRUE)
   } 
   invisible(NULL)
 }
-#' @title Wydzielanie kryteriów i pseudokryteriów
+#' @title Wydzielanie kryteriow i pseudokryteriow
 #' @description
-#' Funkcja wydziala kryteria i pseudokryteria z wektora ciągów znakowych oraz sprawdza ich poprawność.
+#' Funkcja wydziala kryteria i pseudokryteria z wektora ciągów znakowych oraz sprawdza poprawność ich nazw.
 #' @param nazwy wektor ciągów znakowych
 #' @return Funkcja zwraca dwuelementową listę. Pierwszy element o nazwie kryteria zawiera wektor liczb,
-#' który zawiera numery kryterii. Drugi element to wektor numerów pseudokryterii.
+#' który zawiera numery kryteriow. Drugi element to wektor numerów pseudokryteriów.
 wydziel_kryteria_pseudokryteria <- function(nazwy){
-  poprawneKrytBy = grepl("^k_id_[0-9]", nazwy)
-  poprawnePseudokrytBy = grepl("^p_id_[0-9]", nazwy)
-  poprawneIinneKryteriaBy =  grepl("^(([[:alnum:]]+)_)+", nazwy)
-  
+  poprawneKrytBy = grepl("^k_[0-9]", nazwy)
+  poprawnePseudokrytBy = grepl("^p_[0-9]", nazwy)
+  poprawneIinneKryteriaBy =  grepl("^[[:alnum:]]+_", nazwy)
   
   if(sum(! poprawneIinneKryteriaBy) > 0){
-    stop("Nazwy nie pasujące do kryteriów i pseudokryteriów: \n",
-         paste(krytNazwyBy[! poprawneIinneKryteriaBy],collapse="\n" ))
+    stop("Nazwy nie pasujące do kryteriów i pseudokryteriów:\n",
+         paste(krytNazwyBy[! poprawneIinneKryteriaBy], collapse="\n" ))
   }
   if(sum(poprawneIinneKryteriaBy & ! poprawneKrytBy & ! poprawnePseudokrytBy) > 0){
-    warning("Niepoprawne nazwy kryteriów: \n",
+    warning("Niepoprawne nazwy kryteriów:\n",
             paste(nazwy[poprawneIinneKryteriaBy & ! poprawneKrytBy & ! poprawnePseudokrytBy], collapse="\n" ))
   }
   
-  numery = as.numeric( sub("^(([[:alnum:]]+)_)+", "", nazwy) )
+  numery = as.numeric( sub("^[[:alnum:]]+_", "", nazwy) )
   
   kryteria = numery[poprawneIinneKryteriaBy & ! poprawnePseudokrytBy]
   pseudokryteria = numery[poprawnePseudokrytBy]
   
-  return(list(kryteria=kryteria,pseudokryteria = pseudokryteria))
+  return(list(kryteria=kryteria, pseudokryteria = pseudokryteria))
 }
